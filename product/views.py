e9d977da51d8c0a814304d21856747176d87e3f8
Marko Miseljic <marko.miseljic.14@gmail.com>
2021-02-09 13:37:39 +0100

    search and fixed urls

--- product/views.py
+++ product/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from .models import Product, ProductImages, Category
 from django.core.paginator import Paginator
-from django.db.models import Count
+from django.db.models import Count, Q
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
-def productlist(request):
+def productlist(request, category_slug=None):
+    category = None
     productlist = Product.objects.all()
     categorylist = Category.objects.annotate(total_products=Count('product'))
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        productlist = productlist.filter(category=category)
+
+    search_query = request.GET.get('q')
+    if search_query:
+        productlist = productlist.filter(
+            Q(name__icontains=search_query) |
+            Q(description__icontains=search_query) |
+            Q(condition__icontains=search_query) |
+            Q(brand__brand_name__icontains=search_query) | 
+            Q(category__category_name__icontains=search_query) 
+        )
 
     paginator = Paginator(productlist, 10)
 
@@ -16,7 +32,8 @@ def productlist(request):
 
     context = {
         'product_list': productlist,
-        'category_list': categorylist
+        'category_list': categorylist,
+        'category': category
     }
 
     template = 'product/product_list.html'
@@ -25,7 +42,7 @@ def productlist(request):
 
 
 def productdetail(request, product_slug):
-    productdetail = Product.objects.get(slug=product_slug)
+    productdetail = get_object_or_404(Product, slug=product_slug)
     productimages = ProductImages.objects.filter(product=productdetail)
     template = 'product/product_detail.html'
     context = {
